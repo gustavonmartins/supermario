360faef0e3b20f04891f45aec38fb16c528f865d
namoscagnm <martins.yo@gmail.com>
2018-04-26 21:16:53 +0200

    Part 13

--- core/src/com/namosca/game/MarioBros.java
+++ core/src/com/namosca/game/MarioBros.java
@@ -11,8 +11,16 @@ import com.namosca.game.Screens.PlayScreen;
 @SuppressWarnings("unused")
 public class MarioBros extends Game {
 	public static final int V_WIDTH = 400;
-	public static final int V_HEIGHT = 280;
+	public static final int V_HEIGHT = 208;
 	public static final float PPM=100;
+	
+	public static final short DEFAULT_BIT = 1;
+	public static final short MARIO_BIT = 2;
+	public static final short BRICK_BIT = 4;
+	public static final short COIN_BIT = 8;
+	public static final short DESTROYED_BIT = 16;
+	
+	
 	public SpriteBatch batch;
 	
 	@Override
