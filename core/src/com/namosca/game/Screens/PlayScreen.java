bf10c6dafa6213ffa2e5f5cf87c90fefad6bbe0e
namoscagnm <martins.yo@gmail.com>
2018-04-26 21:23:10 +0200

    Part 14

--- core/src/com/namosca/game/Screens/PlayScreen.java
+++ core/src/com/namosca/game/Screens/PlayScreen.java
@@ -100,6 +100,8 @@ public class PlayScreen implements Screen {
 		world.step(1/60f, 6, 2);
 		player.update(dt);
 		
+		hud.update(dt);
+		
 		gamecam.position.x=player.b2body.getPosition().x;
 		
 		gamecam.update();
--- core/src/com/namosca/game/Sprites/Brick.java
+++ core/src/com/namosca/game/Sprites/Brick.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
 import com.namosca.game.MarioBros;
+import com.namosca.game.Scenes.Hud;
 
 public class Brick extends InteractiveTileObject{
 	public Brick(World world, TiledMap map, Rectangle bounds) {
@@ -18,6 +19,7 @@ public class Brick extends InteractiveTileObject{
 		Gdx.app.log("Brick", "collision");
 		setCategoryFilter(MarioBros.DESTROYED_BIT);
 		getCell().setTile(null);
+		Hud.addScore(200);
 		
 	}
 
