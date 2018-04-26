7c5478c712196d2c93f73b5b11a16db7d2416bd8
namoscagnm <martins.yo@gmail.com>
2018-04-26 21:26:56 +0200

    Part 14

--- core/src/com/namosca/game/Sprites/Coin.java
+++ core/src/com/namosca/game/Sprites/Coin.java
@@ -2,14 +2,21 @@ package com.namosca.game.Sprites;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
 import com.namosca.game.MarioBros;
+import com.namosca.game.Scenes.Hud;
 
 
 public class Coin extends InteractiveTileObject{
+	private static TiledMapTileSet tileSet;
+	private final int BLANK_COIN=28;
+	
+	
 	public Coin(World world, TiledMap map, Rectangle bounds) {
 		super(world, map, bounds);
+		tileSet  = map.getTileSets().getTileSet("tileset_gutter");
 		fixture.setUserData(this);
 		setCategoryFilter(MarioBros.COIN_BIT);
 	}
@@ -17,6 +24,8 @@ public class Coin extends InteractiveTileObject{
 	@Override
 	public void onHeadHit() {
 		Gdx.app.log("Coin", "collision");
+		getCell().setTile(tileSet.getTile(BLANK_COIN));
+		Hud.addScore(100);
 		
 	}
 
